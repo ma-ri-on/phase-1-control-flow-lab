ef0d6fb9e11b56aa169fa7c5ce36e365d2119af7
Marion <marionasukuku@gmail.com>
2022-08-20 15:50:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(ride){
+    // Write your code here!
+    if(ride <= 400){
+      return "This one is on me!";
+    }   else if(ride >2000 && ride<=2500 ){
+      return "I will gladly take your thirty bucks.";
+    } else if(ride>2500){
+      return "No can do.";
+    }
+  }
+  
+  
+  function ternaryCheckCity(city){
+    // Write your code here!
+    if(city === 'NYC'){
+      return "Ok, sounds good."; 
+    }else{
+      return "No go.";
+    }
+  }
+  
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+  
+  }
